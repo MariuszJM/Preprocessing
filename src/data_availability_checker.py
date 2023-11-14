4b448c915d51c960827c032f9023cf7248337a73
MariuszMJ <mariusz.michna.j@gmail.com>
2023-11-14 11:14:18 +0100

    get_test_cycles_and_scenarios_from_file works with tests

--- src/data_availability_checker.py
+++ src/data_availability_checker.py
@@ -1,13 +1,21 @@
 import os
+import pandas as pd
 
 
-def get_test_cycles_and_scenarios_from_file(file_path):
-    if os.path.exists(file_path):
-        return {
-            "test_cycle1": ["scenario1", "scenario2"],
-            "test_cycle2": ["scenario3", "scenario4"]
-        }
-    return {}
+def get_test_cycles_and_scenarios_from_file(output_file):
+    """
+    Retrieve the structure of test cycles and their associated scenarios from a CSV file.
+
+    :param output_file: The path to the output CSV file.
+    :return: A dictionary with test cycles as keys and lists of scenarios as values.
+    """
+    if os.path.exists(output_file):
+        df = pd.read_csv(output_file)
+        test_cycles_scenarios = df.groupby('TEST CYCLE')['SCENARIO'].unique().to_dict()
+        test_cycles_scenarios = {tc: scenarios.tolist() for tc, scenarios in test_cycles_scenarios.items()}
+        return test_cycles_scenarios
+    else:
+        return {}
 
 
 def get_test_cycles_and_scenarios_from_db():
---
+++ tests/test_data_availability_checker.py
@@ -0,0 +1,18 @@
+import unittest
+
+from src.data_availability_checker import get_test_cycles_and_scenarios_from_file
+
+
+class TestGetDataFromCSV(unittest.TestCase):
+
+    def setUp(self):
+        self.test_output_file = 'test_output.csv'
+
+    def test_get_test_cycles_and_scenarios_from_file(self):
+        expected_output = {'CCAR_2023': ['Historic', 'Internal Baseline', 'Severely Adverse']}
+        result = get_test_cycles_and_scenarios_from_file(self.test_output_file)
+        self.assertEqual(result, expected_output, "The function did not return the expected dictionary structure.")
+
+    def test_get_test_cycles_and_scenarios_from_file_nonexistent(self):
+        result = get_test_cycles_and_scenarios_from_file('nonexistent_file.csv')
+        self.assertEqual(result, {}, "The function should return an empty dictionary for non-existent files.")
